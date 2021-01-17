631caf584650fac74aff748d182ce5289b5f2ec1
Oskari Helenius <Oskari.Helenius@edu.turkuamk.fi>
2021-01-17 17:33:01 +0200

    filu kasi

--- Exercise1 p8.py
+++ Exercise1 p8.py
@@ -1,27 +1,24 @@
-class Virhe(Error):
-    print("Kivi, Paperi tai Sakset kiitos :)")
-    pass
-
 peluripisteet = 0
+pclista = 0
 
+def laskuri(f):
+    global pclista
+    global peluripisteet
+    if f == 0:
+        peluripisteet += 1
+    elif f == 1:
+        pclista += 1
+    elif (peluripisteet == 3): 
+        print("Geimeri voitti")
+        print("Resetoi peli")
+        peluripisteet = 0
+        pclista = 0
+    elif (pclista == 3): 
+        print("Tietskari voitti")
+        print("Resetoi peli")
+        peluripisteet = 0
+        pclista = 0
 
-def laskuri1(f):
-    while (peluripisteet < 3): 
-        if f > 0:
-            peluripisteet += 1
-    print("Geimeri voitti")
-
-
-pclista = 0             
-                    
-            
-def pclaskuri(i): 
-    while (pclista < 3):
-        if i > 0:
-            pclista += 1
-    print("Tietskari voitti")
-    
-            
 from random import randint 
 
 l = ["Kivi", "Paperi", "Sakset"]
@@ -36,43 +33,36 @@ def rps(tietskari):
     geimeri = False 
     
     while geimeri == False:
-        try:
+        
             geimeri = input('"Kivi", "Paperi", "Sakset? "') 
-            if geimeri != ("Kivi" or "Paperi" or "Sakset"):
-                raise Virhe
+            print(geimeri)
+           # if geimeri != "Kivi" or geimeri != "Paperi" or geimeri != "Sakset": 
             if geimeri == tietskari:
                 print ("Tasuri!")
             elif geimeri == "Kivi":
                 if tietskari == "Paperi": 
                     print(h)
-                    pclaskuri(1)
+                    laskuri(1)
                 else:
                     print(v)
-                    laskuri1(1)
+                    laskuri(0)
             elif geimeri == "Paperi":
                 if tietskari == "Sakset":
                     print(h)
-                    pclaskuri(1)
+                    laskuri(1)
                 else:
                     print(v)
-                    laskuri1(1)
+                    laskuri(0)
             elif geimeri == "Sakset":
                 if tietskari == "Kivi":
                     print (h)
-                    pclaskuri(1)
+                    laskuri(1)
                 else: 
                     print(v)
-                    laskuri1(1)
+                    laskuri(0)
                 
-                geimeri = False
-                rps(l[randint(0,2)])
+            geimeri = False
+            rps(l[randint(0,2)])
     
+
 rps(l[randint(0,2)])
-            
-                
-            
-                
-            
-            
-    
-        
